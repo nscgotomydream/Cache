2c89bf8d8e785bc1ea2b3fd8bc862d8fc1d44262
nscgotomydream <18810487612@163.com>
2016-06-15 18:52:14 +0800

    1.0.0
    1.0.0

--- test/test2.php
+++ test/test2.php
@@ -3,25 +3,15 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2016-06-15
- * Time: 14:25
+ * Time: 18:44
  */
 include("../src/Xcache/XcacheInterface.php");
 include("../src/Xcache/Xcache.php");
 
 $xcache = \Nsc\Xcache\Xcache::getInstance();
-$a = 'nsc';
-//数组
+$a = 2;
 $arr = array(1,2,3,4,5,6);
-$arr2 = array(7,8,9);
 $xcache->set($a,$arr);
-//$xcache->inc($a,$arr2);
 var_dump($xcache->get($a));
-$xcache->dec($a,6);
+$xcache->clear(1);
 var_dump($xcache->get($a));
-//字符串
-$b = 'ceshi';
-$xcache->set($b,'abcd');
-$xcache->inc($b,'efg');
-var_dump($xcache->get($b));
-$xcache->dec($b,'fg');
-var_dump($xcache->get($b));
