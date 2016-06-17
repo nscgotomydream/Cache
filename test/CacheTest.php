c7f8faca65b0c99f8d744fc475fe84ecdb4c4d81
nscgotomydream <18810487612@163.com>
2016-06-17 17:02:38 +0800

    1.0.0
    1.0.0

--- test/CacheTest.php
+++ test/CacheTest.php
@@ -8,5 +8,65 @@
  */
 class CacheTest extends PHPUnit_Framework_TestCase
 {
+    private $config = [
+        'GROUP'  => 'ceShi'
+    ];
+    private $cache;
+    protected function setUp()
+    {
+        $this->cache = new Nsc\Cache\Cache($this->config);
+    }
+    public static function provider()
+    {
+        return array(
+            array('nsc', 23, 1800),
+            array('c', 8, 1800),
+            array('a', [1,2,3,4,5,6], 1800),
+            array('b', "hello world", 1800),
+        );
+    }
+    /**
+     * @dataProvider provider
+     */
+    public function testSet($key,$value,$expire){
+        $this->cache->set($key,$value,$expire);
+    }
+    /**
+     * @dataProvider provider
+     * @depends testSet
+     */
+    public function testGet($key){
+        $this->assertEquals(1,$this->cache->exists($key));
+        $this->cache->get($key);
+    }
+    /**
+     * @depends testSet
+     */
+    public function testInc(){
+        $this->cache->inc('nsc',2);
+        $this->assertEquals(25,$this->cache->get('nsc'));
+    }
+    /**
+     * @depends testSet
+     */
+    public function testDec(){
+        $this->cache->dec('c');
+        $this->assertEquals(7,$this->cache->get('c'));
+    }
+    /**
+     * @dataProvider provider
+     * @depends testSet
+     */
+    public function testClear($key){
+        $this->cache->clear();
+        $this->assertEquals(0,$this->cache->exists($key));
+    }
+    /**
+     * @depends testSet
+     */
+    public function testDelete(){
+        $this->cache->delete('nsc');
+        $this->assertEquals(0,$this->cache->exists('nsc'));
+    }
 
 }
